f068d4cae4c6dfda6eecba4ccbc72c413f6ca2b9
Sujith Kumar <l.sujithkumar154@gmail.com>
2025-12-29 09:23:24 +0530

    2Darray.py

---
+++ 2Darray.py
@@ -0,0 +1,6 @@
+import numpy as np
+mat = np.array([[1, 2], [3, 4]])
+print("Transpose of matrix:\n", np.transpose(mat))
+print("Sum of all elements:", np.sum(mat))
+print("Minimum element:", np.min(mat))
+print("Shape of matrix:", mat.shape)
